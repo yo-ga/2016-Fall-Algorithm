f1f748522df1c65131f9cffb322a57e76655f032
YOGA <art96962006@gmail.com>
2016-10-06 16:58:34 +0800

    Get AC with fibonacci.

--- Program HW/Week2/fibonacci.cpp
+++ Program HW/Week2/fibonacci.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 long fibonacci(long n){
+	long ans =0;
 	if(n==0 || n==1)
-		return n;
+        ans=n;
 	else
-		return fibonacci(n-1)+fibonacci(n-2);
+		ans= fibonacci(n-1)+fibonacci(n-2);
+    return ans;
 }
 int main(){
-	int num=0;
-	long fibo=0;
-	scanf("%d",&num);
+	long num=1;
+	long fibo;
+	scanf("%ld",&num);
 	for(int i=0;i<num;i++){
-		scanf("%lld",&fibo);
-		printf("%lld\n",fibonacci(fibo));
+		scanf("%ld",&fibo);
+		printf("%ld\n",fibonacci(fibo));
 	}
 	return 0;
 }

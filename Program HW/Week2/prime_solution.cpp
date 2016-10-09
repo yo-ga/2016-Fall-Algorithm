e675d7ca321038d6a5d786be65ea2c7bdfe55839
YOGA <art96962006@gmail.com>
2016-10-09 09:35:50 +0800

    Remove some sub-function.

--- Program HW/Week2/prime_solution.cpp
+++ Program HW/Week2/prime_solution.cpp
@@ -15,56 +15,6 @@ void make_prime(bool* prime,int len){
 	}
 	return;
 }
-void print_stack(stack<int> bag){
-    stack<int> pool;
-    while(!bag.empty()){
-        pool.push(bag.top());
-        bag.pop();
-        printf("%d%s",pool.top(),bag.empty()?"\n":" ");
-    }
-}
-void put_in_bag(bool prime[],int sum,int bsize){
-    stack<int> mybag;
-    bool ispare=false;
-    int pt=sum,t_sum=0;
-    while(true){
-        while(!prime[pt]){
-            if(pt>1)
-                pt--;
-            else
-                break;
-        }
-        if(pt*(bsize-mybag.size())+t_sum<sum)
-            if(mybag.empty())
-                break;
-            else{
-                pt=mybag.top();
-                mybag.pop();
-                t_sum-=pt;
-                pt--;
-            }
-        else if(bsize*2>sum)
-            break;
-        if(pt+t_sum<sum&&mybag.size()<bsize-1){
-            mybag.push(pt);
-            t_sum+=pt;
-        }
-        else if(pt+t_sum==sum&&mybag.size()==bsize-1){
-            mybag.push(pt);
-            print_stack(mybag);
-            ispare=true;
-            mybag.pop();
-            pt--;
-        }
-        else{ //pt+t_sum<sum&&mybag.size()==bsize-1
-            pt--;
-        }
-    }
-    if(ispare)
-        printf("\n");
-    else
-        printf("0\n\n");
-}
 int main(){
 	int n,s,num,sum;
 	scanf("%d",&num);
@@ -74,8 +24,8 @@ int main(){
         for (int x=0;x<s+1;x++){
             prime[x]=true;
         }
-        make_prime(prime,s+1);
-        put_in_bag(prime,s,n);
+
         printf("\n");
 	}
+	return 0;
 }

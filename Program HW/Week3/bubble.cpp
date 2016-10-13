f00590afbb10da0963d88111efbb78fb7f31043a
YOGA <art96962006@gmail.com>
2016-10-13 11:06:53 +0800

    Create "bubble.cpp".

---
+++ Program HW/Week3/bubble.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int num,len;
+int bub[50];
+int main(){
+    scanf("%d",&num);
+    for(int n=0;n<num;n++){
+        scanf("%d",&len);
+        for(int l=0;l<len;l++){
+
+        }
+    }
+}

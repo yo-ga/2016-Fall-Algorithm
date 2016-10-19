637dccfeef41058565aaeffafed7944cd293f137
YOGA <art96962006@gmail.com>
2016-10-19 22:42:29 +0800

    Finish the max subarray.

--- Program HW/Week4/max_subarray.cpp
+++ Program HW/Week4/max_subarray.cpp
@@ -1,1 +1,22 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int n,num,tem,Max;
+int main(){
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        tem=0;
+        Max=0;
+        for(int j=0;j<10;j++){
+            scanf("%d",&num);
+            tem+=num;
+            if(tem>Max)
+                Max=tem;
+            else if(tem<0){
+                Max=0;
+                tem=0;
+            }
+        }
+        printf("%d",Max);
+    }
+    return 0;
+}
